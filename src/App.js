89a3224b2d8a6e183d6ada54071daa20d384b5ba
fanzhong <fanzhongzeng78@gmail.com>
2020-01-21 10:43:41 -0500

    Added Relationship (import) and fetched all champion data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChampionList from "./Champions/ChampionList.js"
+import NavBar from "./Champions/NavBar.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="Main">
+      <NavBar />
+      <ChampionList />
     </div>
   );
 }
